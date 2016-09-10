5c7872d8bbc565ded5b97549696b61c91e205feb
brenteritou <beritou@gmail.com>
2016-09-09 21:56:57 -0400

    Remove tests no longer needed

--- src/test/scala/mfp/report/ReportServiceSpec.scala
+++ src/test/scala/mfp/report/ReportServiceSpec.scala
@@ -18,45 +18,5 @@ class ReportServiceSpec extends ServiceTestBase with Matchers with ScalatestRout
         status should be(StatusCodes.OK)
       }
     }
-
-    "return a greeting for GET requests to the root path" in {
-      Get() ~> smallRoute ~> check {
-        responseAs[String] shouldEqual "Captain on the bridge!"
-      }
-    }
-
-    "return a 'PONG!' response for GET requests to /ping" in {
-      // tests:
-      Get("/ping") ~> smallRoute ~> check {
-        responseAs[String] shouldEqual "PONG!"
-      }
-    }
-
-    "leave GET requests to other paths unhandled" in {
-      // tests:
-      Get("/kermit") ~> smallRoute ~> check {
-        handled shouldBe false
-      }
-    }
-
-    "return a MethodNotAllowed error for PUT requests to the root path" in {
-      // tests:
-      Put() ~> Route.seal(smallRoute) ~> check {
-        status === StatusCodes.MethodNotAllowed
-        responseAs[String] shouldEqual "HTTP method not allowed, supported methods: GET"
-      }
-    }
   }
-
-  val smallRoute: Route =
-    get {
-      pathSingleSlash {
-        complete {
-          "Captain on the bridge!"
-        }
-      } ~
-        path("ping") {
-          complete("PONG!")
-        }
-    }
 }
